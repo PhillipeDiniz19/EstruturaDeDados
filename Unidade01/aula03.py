48f60fc9fb61fa00e4abc7a18f0185a0273eedf7
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-05 16:59:44 -0300

    finalizando aula03

--- Unidade01/aula03.py
+++ Unidade01/aula03.py
@@ -33,4 +33,40 @@ insereNoInicio(lista, 5)
 print("Lista contém um único elemento:", lista)
     
 insereNoInicio(lista, 10)
-print("Inserindo um novo elemento:", lista)
+print("Inserindo um novo elemento:", lista) 
+
+# Desafio da aula 03.
+
+class Produto:
+    def __init__(self, codigo=0, proximo_nodo=None):
+        self.codigo = codigo
+        self.proximo = proximo_nodo
+        
+    def __repr__(self):
+        return "%s -> %s" % (self.codigo, self.proximo)
+    
+class ListaEncadeada: 
+    def __init__(self):
+        self.cabeca = None
+    
+    def __repr__(self):
+        return "[ " + str(self.cabeca) + "]"
+    
+    def AddFinal(self, novoCod):
+        novoProduto = Produto(novoCod)
+        if not self.cabeca:
+            self.cabeca = novoProduto
+            return
+        atual = self.cabeca
+        while atual.proximo:
+            atual = atual.proximo
+        atual.proximo = novoProduto
+
+# Testar o funcionamento
+lista = ListaEncadeada()
+print("Lista vazia:", lista)
+lista.AddFinal(1111)
+print(lista)
+lista.AddFinal(2222)
+print(lista)
+
