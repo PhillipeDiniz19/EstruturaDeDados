f938c47c8f092392c8838358854ee9429d6fe56d
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-05 16:21:35 -0300

    unidade 01 aula2 completa

--- Unidade01/aula02.py
+++ Unidade01/aula02.py
@@ -2,5 +2,17 @@
 #Exercicio 01
 Aluno = ["A",20,60,"casa",3]
 print(Aluno[-1])
-
 #Exercicio 02
+
+nomeAluno = ["nome","N1","N2","N3","media"]
+
+nomeAluno[0] = 'Phillipe'
+nomeAluno[1] = 5
+nomeAluno[2] = 7
+nomeAluno[3] = 8
+nomeAluno[4] = 9
+
+#Cálculo da média do aluno
+nomeAluno [5] = (nomeAluno[1] + nomeAluno[2] + nomeAluno[3]+ nomeAluno[4])/4
+
+print(nomeAluno)
